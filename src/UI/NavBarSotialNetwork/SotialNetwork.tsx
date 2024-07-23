061e494e845a6d7f136867e4856fda9991045892
Alex Eremin <ifollowalex@gmail.com>
2024-07-23 16:12:56 +0600

    добавил нормальные иконки соц сетей

--- src/UI/NavBarSotialNetwork/SotialNetwork.tsx
+++ src/UI/NavBarSotialNetwork/SotialNetwork.tsx
@@ -1,19 +1,21 @@
 import s from './SotialNetworks.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp, faVk, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 
 export const SotialNetwork:React.FC = () => {
     return(
         <nav className={s.mediaLinks__box}>
             <a className={s.mediaLink} href="https://wa.me/79994522614" target="blank">
-                wh
-                <FontAwesomeIcon icon={faChartSimple} />
+                <FontAwesomeIcon className={s.mediaLink__fa} icon={faWhatsapp} />
             </a>
-            <a className={s.mediaLink} href="https://vk.com/popo_make" target="blank"> vk</a>
+            <a className={s.mediaLink} href="https://vk.com/popo_make" target="blank"> 
+            <FontAwesomeIcon className={s.mediaLink__fa} icon={faVk} />
+            </a>
            
-            <a className={s.mediaLink} href="https://instagram.com/popo_make?igshid=OGQ5ZDc2ODk2ZA==" target="blank" >inst</a>
+            <a className={s.mediaLink} href="https://instagram.com/popo_make?igshid=OGQ5ZDc2ODk2ZA==" target="blank" >
+            <FontAwesomeIcon className={s.mediaLink__fa} icon={faInstagram} />
+                </a>
             
         </nav>
     )
